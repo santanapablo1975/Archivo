9d5c27b7f46c3f6db7e63ff31a8eff978f6a5276
santanapablo1975 <santanapablo1975@gmail.com>
2016-11-25 10:49:37 -0500

    Add files via upload

---
+++ Archivo/Archivo/src/archivo/Archivo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package archivo;
+
+/**
+ *
+ * @author PC54
+ */
+public class Archivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     new Formulario().setVisible(true);
+    }
+    
+}
